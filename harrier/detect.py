eb2719dee2e4c508896373f5c78c79d43129679f
Al <al@eartoearoak.com>
2015-05-21 19:26:28 +0100

    Harrier: Increase pulse rate tolerance (low battery level causes deviations)

--- harrier/detect.py
+++ harrier/detect.py
@@ -41,7 +41,7 @@ class Detect(object):
     # Pulse width tolerance (+/- %)
     PULSE_WIDTH_TOL = 25
     # Maximum pulse rate deviation (%)
-    PULSE_RATE_DEVIATION = 2
+    PULSE_RATE_DEVIATION = 15
     # Valid pulse rates (Pulses per minute)
     PULSE_RATES = [40, 60, 80]
     # Pulse rate tolerance (+/- Pulses per minute)
