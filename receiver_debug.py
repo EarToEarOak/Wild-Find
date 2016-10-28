a8ff644d965af1fbad42a97f99fb561ee4176404
Al <al@eartoearoak.com>
2016-10-28 13:08:08 +0100

    Add receiver debug utility

---
+++ receiver_debug.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+#
+# Wild Find
+#
+#
+# Copyright 2014 - 2016 Al Brown
+#
+# Wildlife tracking and mapping
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from harrier.receiver_debug import ReceiveDebug
+
+
+def main(argList=None):
+    ReceiveDebug(argList)
+
+
+if __name__ == '__main__':
+    main()
